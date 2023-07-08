a276b95eae70c8f09a423959dab23651b75e7b07
laggu91 <laggu91@gmail.com>
2023-07-08 13:33:19 +0000

    feat: scheduler newrelic transaction
    Merge-request: ORBIT-MR-47
    Merged-by: 구원 라 <laggu91@gmail.com>

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,8 +1,8 @@
 import fastq, { queueAsPromised } from 'fastq'
 import schedule from 'node-schedule'
+import newrelic from 'newrelic'
 
 import Messenger from '@/messenger'
-import { logger } from '@utils/logger'
 import { OrbitModel } from '@/models'
 import { MESSENGER_CONCURRENCY } from '@config'
 
@@ -14,18 +14,18 @@ export default class Scheduler {
     }
 
     private async publish() {
-        logger.info('run publisher')
+        await newrelic.startBackgroundTransaction('scheduler', async () => {
+            const now = new Date()
+            let page = 1
+            while (page) {
+                const orbits = await OrbitModel.findByExecutionTime(page, now)
+                for (const orbit of orbits.docs) {
+                    await this.queue.push(orbit)
+                }
 
-        const now = new Date()
-        let page = 1
-        while (page) {
-            const orbits = await OrbitModel.findByExecutionTime(page, now)
-            for (const orbit of orbits.docs) {
-                await this.queue.push(orbit)
+                page = orbits.nextPage
             }
-
-            page = orbits.nextPage
-        }
+        })
     }
 
     public run() {
