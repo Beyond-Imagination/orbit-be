08529eb214b793819c3c4df6fced9ef57ca2b477
laggu91 <laggu91@gmail.com>
2023-07-23 05:50:39 +0000

    fix: add error log when messenger fail
    Merge-request: ORBIT-MR-52
    Merged-by: 구원 라 <laggu91@gmail.com>

--- src/messenger.ts
+++ src/messenger.ts
@@ -2,15 +2,20 @@ import newrelic from 'newrelic'
 
 import { sendChannelMessage } from '@services/space'
 import { Orbit, Organization } from '@/models'
+import { logger } from '@utils/logger'
 
 export default class Messenger {
     public async handler(arg: Orbit) {
-        newrelic.incrementMetric('messenger')
-        // 실제로 사용자에게 메시지를 전송하는 부분
-        const { organization } = { organization: arg.organization } as { organization: Organization }
-        await sendChannelMessage(organization, arg.channelName, arg.message)
+        try {
+            newrelic.incrementMetric('messenger')
+            // 실제로 사용자에게 메시지를 전송하는 부분
+            const { organization } = { organization: arg.organization } as { organization: Organization }
+            await sendChannelMessage(organization, arg.channelName, arg.message)
 
-        // 다음 메세지 큐에 들어가는 element를 선별하기 위함
-        await arg.updateNextExecutionTime()
+            // 다음 메세지 큐에 들어가는 element를 선별하기 위함
+            await arg.updateNextExecutionTime()
+        } catch (e) {
+            logger.error('messenger fail', { error: e, messageId: arg._id })
+        }
     }
 }
