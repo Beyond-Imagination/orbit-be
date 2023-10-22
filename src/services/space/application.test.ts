2323c4ba72d7e789caee753d05822daa3da29aad
yewon <yewon7648@gmail.com>
2023-10-22 08:30:03 +0000

    fix: type error in application.test.ts
    Merge-request: ORBIT-MR-129
    Merged-by: 예원 박

--- src/services/space/application.test.ts
+++ src/services/space/application.test.ts
@@ -1,17 +1,17 @@
 import { getAccessToken, getPublicKeys } from '@services/space/application'
-import { IOrganizationSecret } from '@/types/space'
 import { Unauthorized } from '@/types/errors'
+import { OrganizationSecret } from '@/models'
 
 jest.setTimeout(30000)
 
 describe('getAccessToken', () => {
     it('should success with valid secret', async () => {
         // local test application secret
-        const secret = {
-            clientId: '8ea1d9ae-1d89-4954-ac67-777724dcab82',
-            clientSecret: '5a9e1896ade629269a0af3200a2187e128e16482b5777a7411c3f21afa79eaa9',
-            serverUrl: 'https://beyond-imagination.jetbrains.space',
-        }
+        const secret = new OrganizationSecret(
+            '8ea1d9ae-1d89-4954-ac67-777724dcab82',
+            '5a9e1896ade629269a0af3200a2187e128e16482b5777a7411c3f21afa79eaa9',
+            'https://beyond-imagination.jetbrains.space',
+        )
         const result = await getAccessToken(secret)
         console.log(result)
         expect(result.expires_in).toEqual(600)
@@ -20,11 +20,7 @@ describe('getAccessToken', () => {
     })
 
     it('should fail with invalid secret', async () => {
-        const secret = {
-            clientId: 'invalid',
-            clientSecret: 'invalid',
-            serverUrl: 'https://beyond-imagination.jetbrains.space',
-        }
+        const secret = new OrganizationSecret('invalid', 'invalid', 'https://beyond-imagination.jetbrains.space')
         await expect(getAccessToken(secret)).rejects.toThrowError(Unauthorized)
     })
 })
@@ -32,22 +28,18 @@ describe('getAccessToken', () => {
 describe('getPublicKeys', () => {
     it('should success with valid secret', async () => {
         // local test application secret
-        const secret: IOrganizationSecret = {
-            clientId: '8ea1d9ae-1d89-4954-ac67-777724dcab82',
-            clientSecret: '5a9e1896ade629269a0af3200a2187e128e16482b5777a7411c3f21afa79eaa9',
-            serverUrl: 'https://beyond-imagination.jetbrains.space',
-        }
+        const secret = new OrganizationSecret(
+            '8ea1d9ae-1d89-4954-ac67-777724dcab82',
+            '5a9e1896ade629269a0af3200a2187e128e16482b5777a7411c3f21afa79eaa9',
+            'https://beyond-imagination.jetbrains.space',
+        )
         const token = 'Bearer ' + (await getAccessToken(secret)).access_token
         const result = await getPublicKeys(secret, token)
         expect(result.keys.length).toBeGreaterThanOrEqual(1)
     })
 
     it('should fail with invalid secret', async () => {
-        const secret = {
-            clientId: 'invalid',
-            clientSecret: 'invalid',
-            serverUrl: 'https://beyond-imagination.jetbrains.space',
-        }
+        const secret = new OrganizationSecret('invalid', 'invalid', 'https://beyond-imagination.jetbrains.space')
         const token = 'invalid'
         await expect(getPublicKeys(secret, token)).rejects.toThrowError(Unauthorized)
     })
