ed43b86f28e626155585b8705adb0729820feda1
laggu91 <laggu91@gmail.com>
2023-11-18 13:37:59 +0000

    feat: access token cache
    Merge-request: ORBIT-MR-151
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/services/space/application.ts
+++ src/services/space/application.ts
@@ -1,7 +1,13 @@
 import fetch from 'node-fetch'
+import { LRUCache } from 'lru-cache'
 
 import { AccessToken, IOrganizationSecret, PublicKeys } from '@/types/space'
 import { Unauthorized } from '@/types/errors'
+
+const accessTokenCache: LRUCache<string, string> = new LRUCache<string, string>({
+    max: 100,
+    ttl: 1000 * 60 * 9, // 9 minutes. space accessToken 이 10분간 유효하기 때문에 9분으로 설정
+})
 
 export async function getAccessToken(secret: IOrganizationSecret): Promise<AccessToken> {
     const url = `${secret.serverUrl}/oauth/token`
@@ -26,8 +32,16 @@ export async function getAccessToken(secret: IOrganizationSecret): Promise<Acces
 }
 
 export async function getBearerToken(secret: IOrganizationSecret): Promise<string> {
+    let token = accessTokenCache.get(secret.clientId)
+    if (token) {
+        return token
+    }
+
     const result = await getAccessToken(secret)
-    return `Bearer ${result.access_token}`
+    token = `Bearer ${result.access_token}`
+
+    accessTokenCache.set(secret.clientId, token)
+    return token
 }
 
 export async function getPublicKeys(secret: IOrganizationSecret, token: string): Promise<PublicKeys> {
