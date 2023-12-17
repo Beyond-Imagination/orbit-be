885e248ffcc92bd980f27e046ff4cf0fd248b4c0
laggu91 <laggu91@gmail.com>
2023-12-17 12:11:59 +0000

    chore: dev deploy script
    Merge-request: ORBIT-MR-167
    Merged-by: 구원 나 <laggu91@gmail.com>

---
+++ .space.kts
@@ -0,0 +1,36 @@
+job("[BE]-develop") {
+    startOn {
+        gitPush {
+            anyBranchMatching {
+                +"develop"
+            }
+        }
+    }
+
+    git {
+        refSpec {
+            +"refs/heads/develop"
+        }
+        depth = UNLIMITED_DEPTH
+    }
+
+    container(displayName = "Push heroku remote", image = "alpine:3.18") {
+        env["HEROKU_API_KEY"] = "{{ project:HEROKU_API_KEY }}"
+
+        shellScript {
+            content = """
+                apk update
+                apk add git
+                echo "Git Version:"
+                git --version
+
+                git switch develop
+                echo "Git Status:"
+                git status
+
+                git remote add heroku https://token:${'$'}HEROKU_API_KEY@git.heroku.com/bi-orbit.git
+                git push heroku develop:main -f
+            """
+        }
+    }
+}
