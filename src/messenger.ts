8e416f7419210d0da33c20320d34f2db8cf9c289
laggu91 <laggu91@gmail.com>
2024-03-03 11:01:05 +0000

    fix: change type assertion on messenger.ts
    Merge-request: ORBIT-MR-182
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/messenger.ts
+++ src/messenger.ts
@@ -5,19 +5,19 @@ import { Orbit, Organization } from '@/models'
 import { logger } from '@utils/logger'
 
 export default class Messenger {
-    public async handler(arg: Orbit) {
+    public async handler(orbit: Orbit) {
         try {
             newrelic.incrementMetric('messenger')
             // 실제로 사용자에게 메시지를 전송하는 부분
-            const { organization } = { organization: arg.organization } as { organization: Organization }
-            const result = await sendChannelMessage(organization, arg.channelName, arg.message)
-            await arg.updateStatus(result)
+            const organization = orbit.organization as Organization
+            const result = await sendChannelMessage(organization, orbit.channelName, orbit.message)
+            await orbit.updateStatus(result)
 
-            // 다음 메세지 큐에 들어가는 element를 선별하기 위함
-            await arg.updateNextExecutionTime()
+            // 다음 메세지 큐에 들어가는 element 를 선별하기 위함
+            await orbit.updateNextExecutionTime()
         } catch (e) {
-            logger.error('messenger fail', { error: e, messageId: arg._id })
-            await arg.updateStatus(false)
+            logger.error('messenger fail', { error: e, messageId: orbit._id })
+            await orbit.updateStatus(false)
         }
     }
 }
