5166b3377b9b497c4393f9f48dce39c739860eea
laggu91 <laggu91@gmail.com>
2024-04-22 14:24:29 +0000

    chore: show automation result on merge request channel
    Merge-request: ORBIT-MR-198
    Merged-by: 구원 나 <laggu91@gmail.com>

--- .space.kts
+++ .space.kts
@@ -12,26 +12,46 @@ job("[BE] Merge Request") {
 
     parallel {
         container(displayName = "build & test", image = "node:alpine") {
+            env["REVIEW_ID"] = "{{ run:review.id }}"
+            env["PROJECT_ID"] = "{{ run:project.id }}"
+            env["SPACE_AUTOMATION_AUTHORIZATION"] = "{{ project:SPACE_AUTOMATION_AUTHORIZATION }}"
+
+            cache {
+                // package.json 의 내용을 해시를 하고 그 값을 캐싱키로 사용
+                // 이를 통해 package.json 이 동일하면 캐시를 사용하도록 유도하고 달라지면 캐시를 새로 만든다
+                // 참고: https://www.jetbrains.com/help/space/cache-files.html#upload-and-reuse-cached-files
+                storeKey = "npm-{{ hashFiles('package.json') }}"
+
+                // Fallback 옵션인데 불필요 할것 같아서 주석처리
+                /*restoreKeys {
+                    +"npm-master"
+                }*/
+
+                // 캐시가 들어갈 디렉토리
+                localPath = "node_modules"
+            }
+
             shellScript {
                 content = """
                     set -e
                     yarn install
+                    yarn reviewer &
                     yarn build
                     yarn test
+                    wait
                 """
             }
         }
 
-        container(displayName = "add reviewer", image = "node:alpine") {
+        container(displayName = "send automation result", image = "gradle:6.1.1-jre11") {
             env["REVIEW_ID"] = "{{ run:review.id }}"
-            env["PROJECT_ID"] = "{{ run:project.id }}"
-            env["SPACE_AUTOMATION_AUTHORIZATION"] = "{{ project:SPACE_AUTOMATION_AUTHORIZATION }}"
-            shellScript {
-                content = """
-                    set -e
-                    yarn install
-                    yarn reviewer
-                """
+            kotlinScript { api ->
+                api.space().chats.messages.sendMessage(
+                    channel = ChannelIdentifier.Review(ReviewIdentifier.Id(System.getenv("REVIEW_ID"))),
+                    content = ChatMessage.Text(
+                        text = api.executionUrl()
+                    )
+                )
             }
         }
     }
